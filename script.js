b7f5609b75e1c1cfe519a76c2d3ef1760bf75f55
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-06 10:41:41 -0600

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const h2 = document.createElement("h2");
-
-h2.textcontent = "This content added by JavaScript";
+h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
